cfb28aee6b25d04d19a0f298320521073fdb9877
Manu <manu@mercenaries.ai>
2023-11-07 16:57:00 +0800

    fixed a bug where the whole image data was being added to the database when updating the expiration time on the image

--- scripts/server/files.js
+++ scripts/server/files.js
@@ -18,8 +18,13 @@ const script = {
       user: ctx.userId.toLowerCase(),
       includeUnowned: payload.includeUnowned ?? true
     }
-    let files =  ctx.app.cdn.kvStorage.getAny('file.',undefined,{limit,cursor, expiryType, owner, tags}).map((file) => {
-
+    let files = await Promise.all(ctx.app.cdn.kvStorage.getAny('file.',undefined,{limit,cursor, expiryType, owner, tags}).map(async (file) => {
+      if (file.value.data)
+      {
+        // hot fix to remove data that snuck in there because of the setexpiry bug
+        delete file.value.data;
+        await ctx.app.cdn.updateFileEntry(file.value)
+      }
       if (file.value.fid)
       {
         file.value.url = '/fid/' + file.value.fid
@@ -31,6 +36,7 @@ const script = {
 
       return {...file.value, seq: file.seq, tags: file.tags.map((tag) => tag.replace('#tag.', '') ||[] ) }
     })
+    );
 
 
     return {
